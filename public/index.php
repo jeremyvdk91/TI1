dcc2ac617019053eeaa74b1759e1356f0ef7de4a
jeremyvdk91 <vanderkelen.jer@gmail.com>
2025-02-19 10:05:30 +0100

    commit2

--- public/index.php
+++ public/index.php
@@ -1,2 +1,47 @@
 <?php
-echo "hello word";
+
+if(isset($_GET['p'])){
+
+    // création d'un switch qui va vérifier la variable get p
+    switch($_GET['p']){
+        case "accueil":
+            $title ="Accueil";
+            include "../templates/accueil.php";
+            break;
+        case "geo":
+            $title="Geographie";
+            include "../templates/geographie.php";
+            break;
+         case "hist":
+            $title="Histoire";
+            include "../templates/histoire.php";
+            break;
+        case "cult":
+            $title="Culture";
+            include "../templates/culture.php";
+            break;
+        case "gal":
+            $title="Galerie";
+            include "../templates/galerie.php";
+            break;
+        case "form":
+            $title="Contact";
+            include "../templates/contact.php";
+            break;
+        case "link":
+            $title="Liens";
+            include "../templates/liens.php";
+            break;
+
+  
+        // variable p pas dans le switch
+        default:
+            include "../templates/page-404.php";
+       
+    }
+
+// sinon nous sommes sur l'accueil    
+}else{
+    // Appel de la vue
+    include "../templates/accueil.php";
+}
---
+++ templates/inc/menu.php
@@ -0,0 +1,27 @@
+<?php
+#templates\inc\menu.php
+?>
+        <ul>
+          <li>
+            <a href="">Accueil</a>
+          </li>
+          <li>
+            <a href="./?p=geo">Geographie</a>
+          </li>
+          <li>
+            <a href="./?p=hist">Histoire</a>
+          </li>
+          <li>
+            <a href="./?p=cult">Culture</a>
+          </li>
+          <li>
+            <a href="./?p=gal">Galerie</a>
+          </li>
+          <li>
+            <a href="./?p=form">Contact</a>
+          </li>
+          <li>
+            <a href="./?p=link">Liens</a>
+          </li>
+
+        </ul>
